d461f32d914e82994ed599d2084f5f4502479e6c
Erick Barros <erickbarrosrezende@gmail.com>
2023-04-15 14:48:15 -0300

    First commit.

---
+++ src/screens/home/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export const homeScreen = () => {
+  return (
+    <div className="home-screen-container">
+      <div className="home-screen-content"></div>
+    </div>
+  );
+};
